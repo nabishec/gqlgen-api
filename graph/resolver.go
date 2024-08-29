cdc663276ad3ab3a25402167a9524222cb329dbb
nabishec <nabishec@mail.ru>
2024-08-30 00:50:49 +0300

    first step

---
+++ graph/resolver.go
@@ -0,0 +1,19 @@
+package graph
+
+import "graphapi/graph/model"
+
+// This file will not be regenerated automatically.
+//
+// It serves as dependency injection for your app, add any dependencies you require here.
+
+type Resolver struct {
+	posts    map[string]*model.Post
+	comments map[string][]*model.Comment
+}
+
+func NewResolver() *Resolver {
+	return &Resolver{
+		posts:    make(map[string]*model.Post),
+		comments: make(map[string][]*model.Comment),
+	}
+}
